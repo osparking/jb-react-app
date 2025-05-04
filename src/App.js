21d67c635c42118ebcfd37c7d1dc38255ce578c5
박종범 <jbpark03@gmail.com>
2025-05-04 13:58:14 +0900

    send and use boolean prop in parent, child compo

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>개발자 프로필</h1>
-      <Profile name="이재강" age="12" num="100" />
-      <Profile name="김태호" age="45" num={2025} />
+      <Profile age="12" num="100" isReged={true} />
+      <Profile age="45" num={2025} isReged={false} />
     </div>
   );
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Profile = (props) => {
   return (
     <>
-      <h3>이름: {props.name}</h3>
       <h3>연령: {props.age}</h3>
       <h3>수량: {props.num + 1}</h3>
+      <h3>유저: {props.isReged ? "회원" : "비회원"}</h3>
     </>
   );
 }
