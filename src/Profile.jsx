d24634ac0403094ee9ca717d695a8e04109f8154
박종범 <jbpark03@gmail.com>
2025-05-04 18:29:14 +0900

    apply destructuring in passing props

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Profile = (props) => {
+const Profile = ({age, num, isReged}) => {
   return (
     <>
-      <h3>연령: {props.age}</h3>
-      <h3>수량: {props.num + 1}</h3>
-      <h3>유저: {props.isReged ? "회원" : "비회원"}</h3>
+      <h3>연령: {age}</h3>
+      <h3>수량: {num + 1}</h3>
+      <h3>유저: {isReged ? "회원" : "비회원"}</h3>
     </>
   );
 }
