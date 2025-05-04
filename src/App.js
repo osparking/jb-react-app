37cc88a9f95a11e6993be29a6b5438c45527fbe6
박종범 <jbpark03@gmail.com>
2025-05-04 18:24:02 +0900

    assign members of object to const group

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Profile from './Profile';
 
 function App() {
+  const developer1 = { age: "12", num: "100", isReged:true };
+  const {age, num, isReged} = developer1;
   return (
     <div className="App">
       <h1>개발자 프로필</h1>
-      <Profile age="12" num="100" isReged={true} />
+      <Profile age={age} num={num} isReged={isReged} />
       <Profile age="45" num={2025} isReged={false} />
     </div>
   );
