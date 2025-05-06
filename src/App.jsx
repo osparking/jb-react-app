6b8893596568b2ff2bdda834188bd6ff2b4c49f0
박종범 <jbpark03@gmail.com>
2025-05-06 15:02:56 +0900

    spread key-value pair of object

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,27 @@ function App() {
     alert(`당신은 '${hobby}' 를 선택했어요.`);
   }
 
+  const mrKim = {
+    name: "김씨",
+    age: 45,
+    isReged: false,
+    hobbies: ["축구", "게임", "산책"],
+    onHobbyClick: onHobbyClick
+  }
+
+  const mrLee = {
+    name: "이씨",
+    age: 45,
+    isReged: false,
+    hobbies: ["축구", "바둑", "산책"],
+    onHobbyClick: onHobbyClick
+  }
+
   return (
     <div className="App">
       <h1>개발자 프로필</h1>
-<Profile
-  name="김씨"
-  age={45}
-  isReged={false} 
-  hobbies={["축구", "게임", "산책"]}
-  onHobbyClick={onHobbyClick}/>
-      <Profile
-        name="이씨"
-        age={20}
-        isReged={true} 
-        hobbies={["독서", "여행"]}
-        onHobbyClick={onHobbyClick}/>
+      <Profile {...mrKim} />
+      <Profile {...mrLee} />
     </div>
   );
 }
