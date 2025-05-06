88c014ef39c0ab03ccd998d908a3f8f72c425003
박종범 <jbpark03@gmail.com>
2025-05-06 11:15:55 +0900

    accept func and call back func

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Profile = ({age, num, isReged}) => {
+const Profile = ({age, num, isReged, hobbies, onHobbyClick}) => {
   return (
     <>
       <h3>연령: {age}</h3>
-      <h3>수량: {num + 1}</h3>
+      <h3>내년도: {num + 1}</h3>
       <h3>유저: {isReged ? "회원" : "비회원"}</h3>
+      <ul>
+        {hobbies.map((hobby, idx) => {
+          return (
+            <li
+              key={idx}
+              onClick={() => onHobbyClick(hobby)}>{hobby}
+            </li>
+          )
+        })}
+      </ul>
+
     </>
   );
 }
