dd51449743e9e473a34790ea150a92a270b8a08d
박종범 <jbpark03@gmail.com>
2025-05-06 14:53:12 +0900

    fix Profile to learn spread

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Profile = ({age, num, isReged, hobbies, onHobbyClick}) => {
+const Profile = ({name, age, isReged, hobbies, onHobbyClick}) => {
   return (
     <>
+      <h3>이름: {name}</h3>
       <h3>연령: {age}</h3>
-      <h3>내년도: {num + 1}</h3>
       <h3>유저: {isReged ? "회원" : "비회원"}</h3>
       <ul>
         {hobbies.map((hobby, idx) => {
